e34398cc647becdc57d839ea47f4647ae80c4622
Peter deHaan <peter@deseloper.com>
2023-01-23 15:23:21 -0800

    More tests, more scripts. Multi-grep

--- tests/Login/index.test.js
+++ tests/Login/index.test.js
@@ -2,4 +2,8 @@ describe("Login", () => {
   test("does a thing #pass", async () => {
     expect(true).toBeTruthy();
   });
+
+  test("testing a #unit", async () => {
+    expect(true).toBeTruthy();
+  });
 });
---
+++ tests/Signup/index.test.js
@@ -0,0 +1,8 @@
+describe("Register2", () => {
+  test("I am an #integration test", async () => {
+    expect(true).toBeTruthy();
+  });
+  test("Im a regular test", async () => {
+    expect(true).toBeTruthy();
+  });
+});
